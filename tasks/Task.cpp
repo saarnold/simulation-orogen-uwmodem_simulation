d4d6c1dfffd7e59f582e23dd62b8b6edeaaf7cc8
Joao Neto <joao.neto@fieb.org.br>
2016-06-16 19:23:59 -0300

    Make Task as one way communication.
    Task's input port from transmitter and it's output ports to receiver.
    Double way communication simulation need two instances of Task.
    Remove UWPhysicalMean because of code duplication.
    Include queueSendIM (driver's queue) and queueRawDataOnTheWay,
    simulating the acoustic channel.
    Simulate travel time, bitrate control. Randomly drops some packets or IMs.
    Implement message status for check delivery of IM.

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -8,13 +8,11 @@ using namespace usbl_evologics;
 Task::Task(std::string const& name)
     : TaskBase(name)
 {
-    _interface.set(ETHERNET);
 }
 
 Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
     : TaskBase(name, engine)
 {
-    _interface.set(ETHERNET);
 }
 
 Task::~Task()
@@ -32,7 +30,11 @@ bool Task::configureHook()
     if (! TaskBase::configureHook())
         return false;
 
-    interface = _interface.get();
+    interface = _receiver_interface.get();
+    status_period = base::Time::fromSeconds(1);
+    travel_time = base::Time::fromSeconds(sound_velocity/_distance.get());
+    probability_good_transmission = _probability.get();
+    im_retry = _im_retry.get();
 
     return true;
 }
@@ -45,14 +47,53 @@ bool Task::startHook()
 void Task::updateHook()
 {
     TaskBase::updateHook();
-    base::Time status_period = base::Time::fromSeconds(1);
 
-    // Output status
-   if((base::Time::now() - last_status) > status_period)
-   {
-       last_status = base::Time::now();
-    //    _message_status.write( addStatisticCounters( checkMessageStatus()));
-   }
+    /**
+     * Output status
+     */
+    if((base::Time::now() - last_status) > status_period)
+    {
+        last_status = base::Time::now();
+        // Keep delivery time in im_status
+        MessageStatus status = im_status;
+        status.time = base::Time::now();
+        _message_status.write(status);
+    }
+
+    /**
+     * Transmission of Raw Data
+     */
+    iodrivers_base::RawPacket raw_data_input;
+    while(_raw_data_input.read(raw_data_input) == RTT::NewData)
+        buffer_size += handleRawPacket(raw_data_input, buffer_size);
+
+    while(checkRawDataTransmission())
+        buffer_size -= sendOnePacket();
+
+    while(hasRawData(queueRawPacketOnTheWay))
+    {
+        _raw_data_output.write(queueRawPacketOnTheWay.front());
+        queueRawPacketOnTheWay.pop();
+    }
+
+    /**
+     * Transmission of Instant Message
+     */
+    SendIM send_im;
+    while(_message_input.read(send_im) == RTT::NewData)
+        enqueueSendIM(send_im);
+
+    while(checkIMStatus(im_status))
+    {
+        im_attempts = defineAttempts();
+        im_status = processIM(im_status);
+    }
+
+    DeliveryStatus delivery_status;
+    while( (delivery_status = checkDelivery(im_status, im_attempts)) != PENDING
+            && im_status.status == PENDING)
+        im_status = updateDelivery(im_status, delivery_status);
+
 }
 void Task::errorHook()
 {
@@ -65,4 +106,196 @@ void Task::stopHook()
 void Task::cleanupHook()
 {
     TaskBase::cleanupHook();
+    queueRawPacket = std::queue<iodrivers_base::RawPacket>();
+    queueSendIM = std::queue<usbl_evologics::SendIM>();
+    // queueRawPacketOnTheWay is not empty.
+    // Well, it's on the way, the receiver must deal with this data. ¯\_(ツ)_/¯
+    buffer_size = 0;
+    im_status.status = EMPTY;
+}
+
+
+std::vector<iodrivers_base::RawPacket> Task::breakInTinyPackets(const iodrivers_base::RawPacket &input_data, int packet_size)
+{
+    std::vector<iodrivers_base::RawPacket> output_vector;
+    iodrivers_base::RawPacket output_raw = input_data;
+    for( size_t i=0; i<input_data.data.size(); i+=packet_size)
+    {
+        output_raw.data.clear();
+        if(input_data.data.size() - i < packet_size)
+            output_raw.data = std::vector<uint8_t>(input_data.data.begin()+i, input_data.data.end());
+        else
+            output_raw.data = std::vector<uint8_t>(input_data.data.begin()+i, input_data.data.begin()+i+packet_size);
+        output_vector.push_back(output_raw);
+    }
+    return output_vector;
+}
+
+int Task::handleRawPacket(const iodrivers_base::RawPacket &input_data, int buffer_size)
+{
+    iodrivers_base::RawPacket raw_data_input = input_data;
+    int increment = 0;
+    std::vector<iodrivers_base::RawPacket> tiny_packets;
+    tiny_packets = breakInTinyPackets(raw_data_input, (interface == SERIAL)? 2 : packet_size);
+    for(size_t i=0; i<tiny_packets.size(); i++)
+    {   // Include in buffer as much bytes as possible
+        if(buffer_size+increment >= transmission_buffer_size)
+            continue;
+        increment += tiny_packets[i].data.size();
+        queueRawPacket.push(tiny_packets[i]);
+    }
+    return increment;
+}
+
+bool Task::checkRawDataTransmission(void)
+{
+    if(queueRawPacket.empty())
+        return false;
+    if(queueRawPacketOnTheWay.empty())
+        return true;
+    if(!controlBitRate(queueRawPacket.front().data, queueRawPacketOnTheWay.back().time, raw_bitrate))
+        return false;
+    return true;
+}
+
+int Task::sendOnePacket(void)
+{
+    if(queueRawPacket.empty())
+        return 0;
+    iodrivers_base::RawPacket on_the_way = queueRawPacket.front();
+    on_the_way.time = base::Time::now();
+    // Don't lose packet
+   if(rand() % 100 < probability_good_transmission)
+        queueRawPacketOnTheWay.push(on_the_way);
+    queueRawPacket.pop();
+    return on_the_way.data.size();
+}
+
+bool Task::checkTravelTime(base::Time init_time, base::Time travel_time)
+{
+    return ((base::Time::now() - init_time) >= travel_time);
+}
+
+bool Task::controlBitRate(const std::vector<uint8_t> &data, base::Time last_time, int bitrate)
+{
+    double time_window = (base::Time::now() - last_time).toSeconds();
+    return (data.size()*8/time_window <= bitrate);
+}
+
+bool Task::hasRawData(const std::queue<iodrivers_base::RawPacket> &queuRawData)
+{
+    if(queuRawData.empty())
+        return false;
+    if(!checkTravelTime(queuRawData.front().time, travel_time))
+        return false;
+    return true;
+}
+
+bool Task::checkIMStatus(const MessageStatus &im_status)
+{
+    if(im_status.status == PENDING)
+        return false;
+    if(queueSendIM.empty())
+        return false;
+    return true;
+}
+
+void Task::enqueueSendIM(const SendIM &send_im)
+{
+    // Check size of Message. It can't be bigger than MAX_MSG_SIZE, according device's manual.
+    if(send_im.buffer.size() > MAX_MSG_SIZE)
+    {
+        if(state() != HUGE_INSTANT_MESSAGE)
+            state(HUGE_INSTANT_MESSAGE);
+        RTT::log(RTT::Error) << "Usbl_evologics Task.cpp. HUGE_INSTANT_MESSAGE. Message discharged: \""
+                             << UsblParser::printBuffer(send_im.buffer)
+                             << "\"" << std::endl;
+        return;
+    }
+    // Check queue size.
+    if(queueSendIM.size() > MAX_QUEUE_MSG_SIZE)
+    {
+        if(state() != FULL_IM_QUEUE)
+            state(FULL_IM_QUEUE);
+        RTT::log(RTT::Error) << "Usbl_evologics Task.cpp. FULL_IM_QUEUE. Message discharged: \""
+                             << UsblParser::printBuffer(send_im.buffer)
+                             << "\"" << std::endl;
+        return;
+    }
+    if(state() != RUNNING)
+        state(RUNNING);
+    queueSendIM.push(send_im);
+}
+
+MessageStatus Task::processIM(const MessageStatus &im_status)
+{
+    if(queueSendIM.empty())
+        throw std::runtime_error("uwmodem_simulation::Task.cpp: processIM: queueSendIM is empty");
+    MessageStatus status = im_status;
+    status.sendIm = queueSendIM.front();
+    status.status = PENDING;
+    // Used to control the transmission time.
+    status.time = base::Time::now();
+    status.messageSent++;
+    queueSendIM.pop();
+    return status;
+}
+
+int Task::defineAttempts(void)
+{
+    int attempts = 1;
+    while ((rand() % 100 > probability_good_transmission) && (attempts <= im_retry))
+        attempts++;
+    return attempts;
+}
+
+DeliveryStatus Task::checkDelivery(const MessageStatus &im_status, int attempts)
+{
+    if(im_status.status != PENDING)
+        return im_status.status;
+    if(!controlBitRate(im_status.sendIm.buffer, im_status.time, im_bitrate))
+        return PENDING;
+    int path = im_status.sendIm.deliveryReport? (attempts*2) : 1;
+    if(!checkTravelTime(im_status.time, travel_time*path))
+        return PENDING;
+    // Lose Instant Message in last try
+    if(rand() % 100 > probability_good_transmission && attempts == im_retry+1)
+        return FAILED;
+    return DELIVERED;
+}
+
+MessageStatus Task::updateDelivery(const MessageStatus &im_status, const DeliveryStatus &delivery)
+{
+    MessageStatus status = im_status;
+    if(delivery == DELIVERED)
+    {
+        _message_output.write(toReceivedIM(im_status.sendIm));
+        status.messageDelivered++;
+    }
+    if(delivery == FAILED)
+        status.messageFailed++;
+
+    status.status = delivery;
+    status.time = base::Time::now();
+    _message_status.write(status);
+
+    if(delivery == DELIVERED)
+        status.status = EMPTY;
+    status.sendIm = SendIM();
+    return status;
+}
+
+ReceiveIM Task::toReceivedIM(const SendIM &send_im)
+{
+    ReceiveIM msg;
+    msg.buffer = send_im.buffer;
+    msg.time = base::Time::now();
+    msg.destination = send_im.destination;
+    msg.source = (send_im.destination == 1)? 2 : 1;
+    msg.deliveryReport = send_im.deliveryReport;
+    msg.duration = msg.time - send_im.time;
+    msg.rssi = -(rand() % 65 + 20);
+    msg.integrity = rand() % 60 + 90;
+    msg.velocity = (rand() % 1000)/1000;
+    return msg;
 }
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -4,6 +4,8 @@
 #define UWMODEM_SIMULATION_TASK_TASK_HPP
 
 #include "uwmodem_simulation/TaskBase.hpp"
+#include "usbl_evologics/Driver.hpp"
+#include <iostream>
 #include <queue>
 
 namespace uwmodem_simulation{
@@ -12,7 +14,7 @@ namespace uwmodem_simulation{
      * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
      * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
      * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
-     * 
+     *
      * \details
      * The name of a TaskContext is primarily defined via:
      \verbatim
@@ -27,30 +29,38 @@ namespace uwmodem_simulation{
 	friend class TaskBase;
     protected:
 
+        base::Time travel_time;
+        static const int sound_velocity = 1500;
+
+        static const int im_bitrate = 976;
+        static const int raw_bitrate = 1600;
+        static const int packet_size = 20;
+        static const int transmission_buffer_size = 16384;
+        int buffer_size;
+
+        int im_retry;
+        int im_attempts;
+        int probability_good_transmission;
         usbl_evologics::InterfaceType interface;
 
-        // Queue of Instant Messages with transmission pending.
-        std::queue<usbl_evologics::SendIM> queuePendingIMs;
-        // Queue of Instant Messages to be transmitted to remote device.
+        // Queue of RawPacket waintg in internal buffer to be transmitted.
+        std::queue<iodrivers_base::RawPacket> queueRawPacket;
+        // Queue of RawPacket been transmitted
+        std::queue<iodrivers_base::RawPacket> queueRawPacketOnTheWay;
+        // Queue of Instant Messages from transmitter to receiver.
         std::queue<usbl_evologics::SendIM> queueSendIM;
-        // Arbitrarily defining a max size for queueSendIM.
+
+        // Instant Message been delivered.
+        usbl_evologics::MessageStatus im_status;
+
         static const size_t MAX_QUEUE_MSG_SIZE = 100;
-
-        // Queue of Packets to be transmitted to remote device.
-        std::queue<iodrivers_base::RawPacket> queueSendRawPacket;
-        // Arbitrarily defining a max size for queueSendRawPacket.
-        static const size_t MAX_QUEUE_RAW_PACKET_SIZE = 50;
-
-        base::Time last_status;
-
         static const size_t MAX_MSG_SIZE = 64;
 
-        // Instant Message counters
-        long long unsigned int counter_message_delivered;
-        long long unsigned int counter_message_failed;
-        long long unsigned int counter_message_received;
-        long long unsigned int counter_message_sent;
-        long long unsigned int counter_message_dropped;
+        base::Time last_write_raw_packet;
+        base::Time last_write_im;
+
+        base::Time status_period;
+        base::Time last_status;
 
     public:
         /** TaskContext constructor for Task
@@ -62,7 +72,7 @@ namespace uwmodem_simulation{
         /** TaskContext constructor for Task
          * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices.
          * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task.
-         * 
+         *
          */
         Task(std::string const& name, RTT::ExecutionEngine* engine);
 
@@ -127,8 +137,110 @@ namespace uwmodem_simulation{
          * before calling start() again.
          */
         void cleanupHook();
+
+        /** Break the input RawPacket in tiny RawPackets
+         *
+         * @param input_data. RawPackt to be transmitted
+         * @param packet_size. Max size of RawPacket
+         * @return vector of RawPacket
+         */
+        std::vector<iodrivers_base::RawPacket> breakInTinyPackets(const iodrivers_base::RawPacket &input_data, int packet_size);
+
+        /** Handle received RawPacket
+         *
+         * @param queueRawPacket. Whre the RawPacket is stored.
+         * @param input_data. Data to be handled
+         * @param buffer_size. Actual filled buffer
+         * @return increment in buffer_size (internal buffer)
+         */
+        int handleRawPacket(const iodrivers_base::RawPacket &input_data, int buffer_size);
+
+        /** Check if is possible to transmit a new RawPacket
+         *
+         * @return true if is possible to transmit a RawPacket
+         */
+        bool checkRawDataTransmission(void);
+
+        /** Send one RawPacket
+         *
+         * Put it from internal buffer and on the way to receiver
+         * @return decrement of buffer_size (internal buffer)
+         */
+        int sendOnePacket(void);
+
+        /** Check if RawPacket arrived at destination
+         *
+         * @param init_time. When the transmission started
+         * @param travel_time. Duration of packet transmission
+         * @return bool. True if packet arrived
+         */
+        bool checkTravelTime(base::Time init_time, base::Time travel_time);
+
+        /** Control bitrate of Rawpacket
+         *
+         * @param input_data, RawPacket to be transmitted
+         * @param last_time, lat time a data was delivered
+         * @param bool. If this rate respect the limit.
+         */
+        bool controlBitRate(const std::vector<uint8_t> &data, base::Time last_time, int bitrate);
+
+        /** Check if RawPacket can be output at receiver
+         *
+         * @param queueRawData
+         * @param last_write. A packet was transmitted.
+         * return bool. Output RawPacket if true
+         */
+        bool hasRawData(const std::queue<iodrivers_base::RawPacket> &queuRawData);
+
+        /** Check if status allows transmission of Instant MessageStatus
+         *
+         * @param im_status. Check status
+         * @return bool. True if IM can be transmitted
+         */
+        bool checkIMStatus(const usbl_evologics::MessageStatus &im_status);
+
+        /** Handle new Instat Message.
+         *
+         * @param send_im. New Instant Message to be enqueued
+         */
+         void enqueueSendIM(const usbl_evologics::SendIM &send_im);
+
+         /** Sent an IM to the remote side and update im_status
+          *
+          * @im_status. To be update
+          * @return update im_status
+          */
+         usbl_evologics::MessageStatus processIM(const usbl_evologics::MessageStatus &im_status);
+
+         /** Define amount of attempts it will be made for delivery an IM
+          *
+          * @return attempts
+          */
+         int defineAttempts(void);
+
+         /** Check delivery of Instant Message
+          *
+          * @param im_status. Track Instant Message, sending time.
+          * @param attempts. How many times the message will be sent till receives an ack.
+          * @return Delivery status.
+          */
+         usbl_evologics::DeliveryStatus checkDelivery(const usbl_evologics::MessageStatus &im_status, int attempts);
+
+         /** Update output ports according delivery os instant message
+          *
+          * @im_status. To be update
+          * @param delivery. FAILED or DELIVERED
+          * @return update im_status
+          */
+         usbl_evologics::MessageStatus updateDelivery(const usbl_evologics::MessageStatus &im_status, const usbl_evologics::DeliveryStatus &delivery);
+
+         /** Turn a SendIM in a ReceiveIM
+          *
+          * @param send_im. Instant Message to be converted
+          * @return ReceiveIM
+          */
+         usbl_evologics::ReceiveIM toReceivedIM(const usbl_evologics::SendIM &send_im);
     };
 }
 
 #endif
-
